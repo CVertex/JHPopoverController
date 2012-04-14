3db5d133fb3215472550877239daeed2ed17c95d
jonhocking <hello@jonhocking.co.uk>
2012-04-14 12:11:52 +0100

    added left check for pop over, started work on the right side check

--- JHPopOverView/JHPopoverViewController/JHPopoverViewController.m
+++ JHPopOverView/JHPopoverViewController/JHPopoverViewController.m
@@ -9,6 +9,9 @@
 #import "JHPopoverViewController.h"
 #import "JHPopoverView.h"
 
+#define kRectBuffer 7
+#define kPopoverRadius 6
+
 @interface JHPopoverViewController ()
 
 @property (strong, nonatomic) UIViewController *viewController;
@@ -36,14 +39,25 @@
     // do some boundary checking for sides here
     
     CGRect convertedRect = rect;    //[view convertRect:rect toView:view];
+    CGFloat xPeak = kRectBuffer + self.contentSize.width/2;
+    CGFloat xOrigin = -kRectBuffer;
+    if (rect.origin.x - (self.contentSize.width/2 - kRectBuffer - kPopoverRadius) < 0 ){
+        xPeak += rect.origin.x - (self.contentSize.width/2 - kPopoverRadius);
+        xOrigin = 0;
+    }
+    else if (rect.origin.x + (self.contentSize.width/2 + kRectBuffer + kPopoverRadius) > view.bounds.size.width ){
+        xPeak += (rect.origin.x + (self.contentSize.width/2 + kRectBuffer + kPopoverRadius)) - view.bounds.size.width;
+        xOrigin = 0;
+    }
     
-    self.popoverView = [[JHPopoverView alloc]initWithFrame:CGRectMake(-7, 0, self.contentSize.width + 14, self.contentSize.height + 14) andXPeak:7 + self.contentSize.width/2];
+    self.popoverView = [[JHPopoverView alloc]initWithFrame:CGRectMake(xOrigin, 0, self.contentSize.width + 2*kRectBuffer, self.contentSize.height + 2*kRectBuffer) andXPeak:xPeak];
     
     CGRect popOverFrame = self.popoverView.frame;
     
     popOverFrame.origin.y += convertedRect.origin.y + convertedRect.size.height;
     
-    popOverFrame.origin.x += (convertedRect.origin.x + convertedRect.size.width/2) - popOverFrame.size.width/2;
+    popOverFrame.origin.x = (convertedRect.origin.x + convertedRect.size.width/2) - xPeak - kRectBuffer;
+    
     
     self.popoverView.frame = popOverFrame;
     
